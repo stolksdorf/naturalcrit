09350fa1e89863c6c990c254b69bcffee250119c
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-12-04 23:50:49 +0100

    basic styles

--- client/naturalcrit/accountPage/accountPage.jsx
+++ client/naturalcrit/accountPage/accountPage.jsx
@@ -2,22 +2,32 @@ import React from 'react';
 const AccountActions = require('../account.actions.js');
 
 const AccountPage = (props) => {
-	console.log(props);
+	return (
+		<div className="accountPage">
+			<div className="details">
+				<h1>Account Page</h1>
+				<br />
+				<p>Username: {props.user.username}</p>
 
-	return <div className="accountPage">
-        <div className="details">
-            <h1>Account Page</h1>
-            <br />
-            <p>Username: {props.user.username}</p>
-
-            <button className="logout" onClick={()=>{
-                if(confirm('Are you sure you want to log out?')) {
-                    AccountActions.removeSession();
-                    window.location = '/';
-                }
-            }}>Log Out</button>
-        </div>
-    </div>;
+				<button className="red delete" disabled>
+					Delete account
+				</button>
+				<button className="orange changeUsername" disabled>
+					Change my username
+				</button>
+			</div>
+			<button
+				className="logout"
+				onClick={() => {
+					if (confirm('Are you sure you want to log out?')) {
+						AccountActions.removeSession();
+						window.location = '/';
+					}
+				}}>
+				Log Out
+			</button>
+		</div>
+	);
 };
 
 module.exports = AccountPage;
