f7da16a85259d398d1e1f4cdd368e6a49d77334a
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-12-04 09:02:13 +0100

    adding favicon

--- client/template.js
+++ client/template.js
@@ -5,7 +5,13 @@ module.exports = function(vitreum){
 	<head>
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css" />
-		<link rel="icon" href="/assets/main/favicon.ico" type="image/x-icon" />
+
+		<!--Favicons for desktop, apple and android apps-->
+		<link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
+		<link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+		<link rel="manifest" href="/assets/favicon/site.webmanifest" />
 
 		<title>NaturalCrit - D&D Tools</title>
 		${vitreum.head}
