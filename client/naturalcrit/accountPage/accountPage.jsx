785dbb1367e47f67a62ee0addd5edb09159eece6
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-12-07 10:19:10 +0100

    `Added logo to account page with NaturalCrit icon and updated CSS for positioning and styling.`

--- client/naturalcrit/accountPage/accountPage.jsx
+++ client/naturalcrit/accountPage/accountPage.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 const AccountActions = require('../account.actions.js');
+const NaturalCritIcon = require('naturalcrit/svg/naturalcrit.svg.jsx');
 
 const AccountPage = (props) => {
 	return (
 		<div className="accountPage">
+			<a className='logo' href='/'>
+				<NaturalCritIcon />
+				<span className='name'>
+					Natural
+					<span className='crit'>Crit</span>
+				</span>
+			</a>
 			<div className="details">
 				<h1>Account Page</h1>
 				<br />
 				<p>
 					<b>Username:</b> {props.user.username}
+					<br />
+					
 				</p>
 				<br />
 				<button
