3ff45c8ff9d92f9bdf8f54ac5dc250dfa51004c5
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-12-14 22:59:30 +0100

    proper removing session

--- client/naturalcrit/account.actions.js
+++ client/naturalcrit/account.actions.js
@@ -96,7 +96,7 @@ const AccountActions = {
 
 	removeSession: () => {
 		const domain = window.domain === '.local.naturalcrit.com' ? 'localhost' : window.domain;
-		document.cookie = `nc_session=${token}; max-age=${60*60*24*365}; path=/; samesite=lax; domain=${domain};`;
+		document.cookie = `nc_session=; expires=Thu, 01 Jan 1970 00:00:01 GMT; samesite=lax; domain=${domain}`;
 	},
 };
 
